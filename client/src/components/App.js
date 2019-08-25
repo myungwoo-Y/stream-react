bdffea9814eabde7d0c3daa04a595901223fc56b
myungwoo-Y <auddn6676@naver.com>
2019-08-25 23:39:37 +0900

    use link tag

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const PageOne = () => {
-    return <div>PageOne</div>
+    return (
+        <div>PageOne
+            <br />
+            <Link to="/pagetwo">Navigate to Page Two</Link>
+        </div> 
+    )
 };
 
 const PageTwo = () => {
     return (
         <div>
             PageTwo
-            <button>Click Me!</button>
+            <button>Click Me!</button> <br />
+            <Link to="/">Navigate to Page One</Link>
+
         </div>
     );
 };
@@ -17,10 +24,10 @@ const PageTwo = () => {
 const App = () => {
     return (
         <div>
-            <BrowserRouter> 
+            <BrowserRouter>
                 <div>
                     <Route path="/" exact component={PageOne} />
-                    <Route path="/pageTwo" exact component={PageTwo} />
+                    <Route path="/pageTwo" component={PageTwo} />
                 </div>
             </BrowserRouter>
         </div>
