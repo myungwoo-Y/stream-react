967c030a9b2027ce9be3afc5d247dd94ca278813
myungwoo-Y <auddn6676@naver.com>
2019-09-02 17:08:42 +0900

    add method to OAuth button evnet

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -16,35 +16,43 @@ class GoogleAuth extends React.Component {
                 this.auth.isSignedIn.listen(this.onAuthChange);
             })
         });
-    }
+    };
 
     onAuthChange = () => {
         this.setState({ isSignedIn: this.auth.isSignedIn.get() });
-    }
+    };
+
+    onSignIn = () => {
+        this.auth.signIn();
+    };
+
+    onSignOut = () => {
+        this.auth.signOut();
+    };
 
     renderAuthButton() {
         if (this.state.isSignedIn === null) {
             return null;
         } else if (this.state.isSignedIn) {
             return (
-                <button className="ui red google button">
+                <button onClick={this.onSignOut} className="ui red google button">
                     <i className="google icon"/>
                     Sign Out
                 </button>
             )
         } else {
             return (
-                <button className="ui red google button">
+                <button onClick={this.onSignIn} className="ui red google button">
                     <i className="google icon"/>
                     Sign In with Google
                 </button>
             )
         }
-    }
+    };
 
     render() {
         return <div>{this.renderAuthButton()}</div>;
-    }
-}
+    };
+};
 
 export default GoogleAuth;
