84217aeaa8021a40519ecb5ff66da8de2bec2d1e
myungwoo-Y <auddn6676@naver.com>
2019-09-02 16:46:26 +0900

    add Auth button , but isn't executed

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 
-class GoogleAuth extends React.Component{
-    state = { isSignedIn: null};
+class GoogleAuth extends React.Component {
+    state = { isSignedIn: null };
     componentDidMount() {
         // gapi는 window 상에서 사용가능한 변수이기때문에
         // 앞에 window를 붙여준다.
@@ -12,23 +12,33 @@ class GoogleAuth extends React.Component{
                 scope: 'email'
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
-                this.setState({isSignedIn: this.auth.isSignedIn.get()})
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
                 this.auth.isSignedIn.listen(this.onAuthChange);
             })
         });
     }
 
     onAuthChange = () => {
-        this.setState({ isSignedIn: this.auth.isSignedIn.get()});
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
     }
 
     renderAuthButton() {
-        if (this.state.isSignedIn === null){
-            return <div>I dont know if we are signed in</div>;
-        }else if(this.state.isSignedIn){
-            return <div>I am signed in!</div>
-        }else{
-            return <div>I am not signed in!</div>            
+        if (this.state.isSignedIn === null) {
+            return null;
+        } else if (this.state.isSignedIn) {
+            return (
+                <button className="ui red google button">
+                    <i className="google icon"/>
+                    Sign Out
+                </button>
+            )
+        } else {
+            return (
+                <button className="ui red google button">
+                    <i className="google icon"/>
+                    Sign In with Google
+                </button>
+            )
         }
     }
 
