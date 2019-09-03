e81743d5079222d5692db807049ced9511c4bec5
myungwoo-Y <auddn6676@naver.com>
2019-09-04 03:44:00 +0900

    add redux-form input in StreamCreate

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component{
+    renderInput(formPops){
+        return <input 
+            onChange={formPops.input.onChange} 
+            value={formPops.input.value}
+            />
+    }
 
-export default StreamCreate;
+    render(){
+        return (
+            <form>
+                <Field name="title" component={this.renderInput}/>
+                <Field name="description" component={this.renderInput}/>
+            </form>
+        )
+    }    
+}
+
+export default reduxForm({
+    form: 'streamCreate '
+})(StreamCreate);
