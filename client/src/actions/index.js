081950fb08a44bc7d1c3e149911522a36c1e8d98
myungwoo-Y <auddn6676@naver.com>
2019-09-17 01:57:07 +0900

    add dispatch process to createStream action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT } from './types';
+import { SIGN_IN, SIGN_OUT,  CREATE_STREAM } from './types';
 
 export const signIn = userId => {
     return{
@@ -15,5 +15,7 @@ export const signOut = () => {
 };
 
 export const createStream = formvalues => async dispatch => {
-    streams.post('/streams', formvalues);
+    const response = await streams.post('/streams', formvalues);
+
+    dispatch({type: CREATE_STREAM, payload: response.data});
 };
