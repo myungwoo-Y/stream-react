5c88abd372bbd37e68ba68e84ffcc0b0bb4effa4
myungwoo-Y <auddn6676@naver.com>
2019-09-17 02:34:03 +0900

    add actions about rest method

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,14 @@
 import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT,  CREATE_STREAM } from './types';
+import { 
+    SIGN_IN, 
+    SIGN_OUT,  
+    CREATE_STREAM,
+    FETCH_STREAMS,
+    FETCH_STREAM,
+    DELETE_STREAM,
+    EDIT_STREAM
+ } from './types';
+import { async } from 'q';
 
 export const signIn = userId => {
     return{
@@ -19,3 +28,26 @@ export const createStream = formvalues => async dispatch => {
 
     dispatch({type: CREATE_STREAM, payload: response.data});
 };
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+    dispatch({type: FETCH_STREAMS, payload: response.data});
+}
+
+export const fetchStream = (id) => async dispatch => {
+    const response = await streams.get(`/streams/${id}`);
+
+    dispatch({type: FETCH_STREAM, payload: response.data});
+}
+
+export const editStream = (id, formvalues) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formvalues);
+
+    dispatch({type: EDIT_STREAM, payload: response.data});
+}
+
+export const deleteStream = (id) => async dispatch => {
+    await streams.delete(`/streams/${id}`);
+
+    dispatch({ type: DELETE_STREAM, payload: id });
+}
