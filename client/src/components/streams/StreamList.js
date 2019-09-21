3ebed98b77ed308d4d0998c6a35767cb42c55401
myungwoo-Y <auddn6676@naver.com>
2019-09-21 10:31:33 +0900

    add id param to url

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {fetchStreams} from '../../actions';
+import {Link} from 'react-router-dom';
 
 class StreamList extends React.Component{
     componentDidMount(){
@@ -11,12 +12,12 @@ class StreamList extends React.Component{
         if ( stream.userId === this.props.currentUserId){
             return(
                 <div className="right floated content">
-                    <button className="ui button primary">
+                    <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
                         Edit
-                    </button>
-                    <button className="ui button negative">
+                    </Link>
+                    <Link className="ui button negative">
                         Delete
-                    </button>
+                    </Link>
                 </div>
             );
         }
