f9a07349ce053b6fb6ada784a202280c0f02a6d5
myungwoo-Y <auddn6676@naver.com>
2019-09-21 20:47:19 +0900

    add modal

---
+++ client/src/components/Modal.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import history from '../history';
+
+const Modal = props => {
+    return ReactDom.createPortal(
+        <div 
+            onClick={() => history.push('/')} 
+            className="ui dimmer modals visible active"
+        >
+            <div 
+                className="ui standard modal visible active"
+                onClick={(e) => e.stopPropagation()}    
+            >
+                <div className="header">Delete Stream</div>
+                <div className="content">
+                    Are you sure you want to delete this stream?
+                </div> 
+                <div className="actions">
+                    <button className="ui primary button">Delete</button>
+                    <button className="ui button">Cancel</button>
+                </div>
+            </div>
+        </div>,
+        document.querySelector('#modal')
+    )
+};
+
+export default Modal;
