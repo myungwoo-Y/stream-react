17334de201e5688bae07e5dd002f4a8d4c1dd194
myungwoo-Y <auddn6676@naver.com>
2019-09-21 11:39:13 +0900

    add fetch in streamEdit

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions'; 
 
-const StreamEdit = () => {
-    return <div>StreamEdit</div>;
+class StreamEdit extends React.Component{
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+    
+    render(){
+        if(!this.props.stream){
+            return <div>Loading...</div>;
+        }
+
+        return( 
+             <div>
+                StreamEdit
+             </div>     
+        )
+    }
 };
 
-export default StreamEdit;
+const mapStateProps = (state, ownProps) => {
+    return { stream : state.streams[ownProps.match.params.id] };
+}
+
+export default connect(mapStateProps, {fetchStream})(StreamEdit);
