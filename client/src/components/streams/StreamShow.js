6e5f6918331989ad882bfa0bafe3c18f18c34d23
myungwoo-Y <auddn6676@naver.com>
2019-09-22 16:45:54 +0900

    fix ref undefined

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -6,12 +6,32 @@ import { fetchStream } from '../../actions';
 class StreamShow extends React.Component{
     constructor(props){
         super(props);
-
         this.videoRef = React.createRef();
     }
 
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);
+        this.buildPlayer();
+    }
+
+    // when update, buildPlyaer should execute.
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+
+    buildPlayer() {
+        if(this.player || !this.props.stream){
+            return;
+        }
+
+        const { id } = this.props.match.params;
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
+        console.log(this.player);    
     }
     
     render() {
